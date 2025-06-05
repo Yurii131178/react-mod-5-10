50372c21f8e7eff618aabc4f6929401c1fdb8de4
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-06-05 23:39:08 +0300

    finish

--- src/components/hooks/useFetchPosts.ts
+++ src/components/hooks/useFetchPosts.ts
@@ -15,7 +15,11 @@ const fetchPosts = async (searchText: string): Promise<Post[]> => {
   const res = await axios.get<FetchPostsResponse>(
     'https://dummyjson.com/posts/search',
     {
-      params: { q: searchText },
+      params: { 
+        q: searchText,
+
+      
+      },
     }
   );
   return res.data.posts;
@@ -25,6 +29,6 @@ export const useFetchPosts = (query: string) => {
   return useQuery({
     queryKey: ['posts', query],
     queryFn: () => fetchPosts(query),
-    keepPreviousData: true,
+    // keepPreviousData: true,
   });
 };
